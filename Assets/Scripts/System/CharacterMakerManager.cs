d49a55048c0e35ba1b498d77e8637f921d1c1417
원자로개구리 <jhjjgu0115@naver.com>
2017-06-26 20:08:51 +0900

    1

--- Assets/Scripts/System/CharacterMakerManager.cs
+++ Assets/Scripts/System/CharacterMakerManager.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Anima2D;
+using CrusadersQuest;
 using UnityEngine;
 
-public class CharacterMakerManager : MonoBehaviour {
+public class CharacterMakerManager : MonoBehaviour
+{
+    public CharacterModel model;
+    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
